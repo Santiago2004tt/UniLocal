d49032dfece5cbaeec4ba5bd0380b86fff76343b
Santiago2004tt <115126538+Santiago2004tt@users.noreply.github.com>
2024-03-05 21:22:24 -0500

    Se sube los test, y se pone al dia el proyecto

--- app/src/test/java/ws/Test.java
+++ app/src/test/java/ws/Test.java
@@ -1,7 +1,54 @@
 package ws;
 
-public class Test {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mongodb.assertions.Assertions;
+import ws.model.documentos.Cliente;
+import ws.model.entidades.Bloqueo;
+import ws.model.enums.EstadoRegistro;
+import ws.repositorio.ClienteRepo;
+
+@SpringBootTest
+public class Test {
+
+    @Autowired
+    private ClienteRepo clienteRepo;
+
+    @org.junit.jupiter.api.Test
+    public void registrarCliente() {
+
+        Cliente cliente = new Cliente("1", "Juan", "123", "juan@", "null", "juanito", "Armenia", EstadoRegistro.ACTIVO,
+                new ArrayList<String>(), new ArrayList<Bloqueo>(), new ArrayList<String>());
+
+        Cliente registro = clienteRepo.save(cliente);
+
+        Assertions.assertNotNull(registro);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void actualizarCliente() {
+
+        Cliente cliente = new Cliente("1", "Juan", "123", "juan@", "null", "juanito", "Armenia", EstadoRegistro.ACTIVO,
+                new ArrayList<String>(), new ArrayList<Bloqueo>(), new ArrayList<String>());
+
+        cliente.setNombre("Jere");
+        Cliente registro = clienteRepo.save(cliente);
+
+        Assertions.assertNotNull(registro);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void registrarClienteVacio() {
+
+        Cliente cliente = new Cliente();
+        cliente.setCodigo("65e7d15e5795ba394144e1a7");
+        cliente.setNombre("Juan");
+
+        Cliente registro = clienteRepo.save(cliente);
+
+        Assertions.assertNotNull(registro);
     }
 }
